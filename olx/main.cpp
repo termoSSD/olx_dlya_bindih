ecf50dfdff9ed1e1528cc76d43c8fcc8eadcc616
termOS <olegmanko479@gmail.com>
2026-02-26 20:18:32 +0200

    Update output to cheque.txt and add medicine info
    Switched output from Text.txt to cheque.txt for product and medicine details. Added headers and logic to main.cpp for medicine discount and cheque printing. Updated project files to reflect new output and file structure. Removed references to Text.txt.

--- olx/main.cpp
+++ olx/main.cpp
@@ -1,25 +1,24 @@
 #include "Header.h"
 #include "Prom.h"
-
-int main() 
-{
-	ofstream onf("Text.txt");
-
-	Prom iPhone("iPhone", 1000);
-
-	iPhone.discaunt25();
-	iPhone.showinfo(onf);
 #include "shop.h"
 #include "tabletki.h"
 
-int main()
+int main() 
 {
 	ofstream onf("cheque.txt");
-    tabletki_ua medicine("Aspirin", 100.0);
 
-    medicine.discount50(); // Зменшить ціну на 10%
+	Prom iPhone("iPhone", 1000);
+	tabletki_ua medicine("Aspirin", 100.0);
+
+	iPhone.discaunt25();
+	iPhone.showinfo(onf);
+	iPhone.showinfo(cout);
+
+	cout << endl;
+
+    medicine.discount50();
     medicine.printCheque(onf); 
-    medicine.printCheque(cout); // Виведе чек у консоль
+    medicine.printCheque(cout);
 
 	onf.close();
 }
